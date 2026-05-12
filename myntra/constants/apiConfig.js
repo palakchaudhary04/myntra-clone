a46f907c9cf8e20c0fdfa19a52f233e67fd461dd
palak <chpalak2020@gmail.com>
2026-05-12 17:31:02 +0530

    api url name change

--- myntra/constants/apiConfig.js
+++ myntra/constants/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Ensure this IP matches your CURRENT laptop IP from 'ipc
-const API_URL = process.env.EXPO_PUBLIC_API_URL || "https://myntra-clone-pcca.vercel.app";
+const API_URL = process.env.EXPO_PUBLIC_API_URL || "jifjnjr://myntra-clone-pcca.vercel.app";
 const api = axios.create({
   baseURL: API_URL,
 });
